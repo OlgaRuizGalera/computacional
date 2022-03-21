829fa150db0cc8e7b2e4e2a62cd5ed547f4a1c0d
OlgaRuizGalera <olgaruizg@correo.ugr.es>
2022-03-21 19:52:58 +0100

    a

---
+++ obligatorio1/prueba.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main (void)
+{
+    int i;
+    cout << "Valor de un entero";
+    cin >> i;
+    cout << i*i;
+    cout << "Toma que chulo pa ti me muero";
+    return 0;
+
+}
