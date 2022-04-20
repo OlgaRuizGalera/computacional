735f7ecb9c84d8c530e56f0e8598ea4934751988
OlgaRuizGalera <olgaruizg@correo.ugr.es>
2022-04-20 17:57:03 +0200

    a

--- obligatorio2/ising.cpp
+++ obligatorio2/ising.cpp
@@ -11,13 +11,13 @@ using namespace std;
 
 int main (void) 
 {
-    int i, j, n, m, o, k, l, N, E, s[10][10];
+    int i, j, n, m, o, k, l, N, E, y, s[50][50];
     double p, f, T;
     ofstream ising;
 
     /*Inicializar cosillas, ¿T es real o entero?*/
     srand(time(NULL));
-    N=10; 
+    N=50; 
     T=4.5;
     o=0;
 
@@ -32,8 +32,68 @@ int main (void)
 
     ising.open ("ising.dat");
 
-    while (o<50*N*N)
+    for (o=0; o<100; o++)
     {
+        for (y=0; y<N*N; y++)
+        {
+
+            n=rand()%N;
+            m=rand()%N;
+
+            if (n+1>N)
+            {
+                if (m==0)
+                {
+                    E=2*s[n][m]*(s[1][m]+s[n-1][m]+s[n][m+1]+s[n][N-1]);
+                }
+                else if (m+1>N)
+                {
+                    E=2*s[n][m]*(s[1][m]+s[n-1][m]+s[n][1]+s[n][m-1]);
+                }
+                else
+                {   
+                    E=2*s[n][m]*(s[1][m]+s[n-1][m]+s[n][m+1]+s[n][m-1]);
+                }
+            }
+            else if (n==0)
+            {
+                if (m==0)
+                {
+                    E=2*s[n][m]*(s[n+1][m]+s[N-1][m]+s[n][1]+s[n][N-1]);
+                }
+                else if (m+1>N)
+                {   
+                    E=2*s[n][m]*(s[n+1][m]+s[N-1][m]+s[n][1]+s[n][m-1]);
+                }
+                else
+                {
+                    E=2*s[n][m]*(s[n+1][m]+s[N-1][m]+s[n][m+1]+s[n][m-1]);
+                } 
+            }
+            else 
+            {
+                if (m==0)
+                {
+                    E=2*s[n][m]*(s[n+1][m]+s[n-1][m]+s[n][m+1]+s[n][N-1]);
+                }
+                else if (m+1>N)
+                {
+                    E=2*s[n][m]*(s[n+1][m]+s[n-1][m]+s[n][1]+s[n][m-1]);
+                }   
+                else
+                {   
+                    E=2*s[n][m]*(s[n+1][m]+s[n-1][m]+s[n][m+1]+s[n][m-1]);
+                }
+            }
+
+
+            minimo(T, E, p);
+            f=rand()%1;
+            if (f<p)
+            {
+                s[n][m]=-s[n][m];
+            }
+        }
         for (k=0; k<N; k++)
         {
             for (l=0; l<N-2; l++)
@@ -44,71 +104,10 @@ int main (void)
             ising<<endl;
         }
         ising<<endl;
-
-        n=rand()%N;
-        m=rand()%N;
-
-        /*Calcular energia con simetria teniendo en cuenta que m supere N y que n supere N*/
-        /*Son if*/
-        if (n+1>N)
-        {
-            if (m==0)
-            {
-                E=2*s[n][m]*(s[1][m]+s[n-1][m]+s[n][m+1]+s[n][N-1]);
-            }
-            else if (m+1>N)
-            {
-                E=2*s[n][m]*(s[1][m]+s[n-1][m]+s[n][1]+s[n][m-1]);
-            }
-            else
-            {
-                E=2*s[n][m]*(s[1][m]+s[n-1][m]+s[n][m+1]+s[n][m-1]);
-            }
-        }
-        else if (n==0)
-        {
-           if (m==0)
-            {
-                E=2*s[n][m]*(s[n+1][m]+s[N-1][m]+s[n][1]+s[n][N-1]);
-            }
-            else if (m+1>N)
-            {
-                E=2*s[n][m]*(s[n+1][m]+s[N-1][m]+s[n][1]+s[n][m-1]);
-            }
-            else
-            {
-                E=2*s[n][m]*(s[n+1][m]+s[N-1][m]+s[n][m+1]+s[n][m-1]);
-            } 
-        }
-        else 
-        {
-            if (m==0)
-            {
-                E=2*s[n][m]*(s[n+1][m]+s[n-1][m]+s[n][m+1]+s[n][N-1]);
-            }
-            else if (m+1>N)
-            {
-                E=2*s[n][m]*(s[n+1][m]+s[n-1][m]+s[n][1]+s[n][m-1]);
-            }
-            else
-            {
-                E=2*s[n][m]*(s[n+1][m]+s[n-1][m]+s[n][m+1]+s[n][m-1]);
-            }
-        }
-
-
-        minimo(T, E, p);
-        f=rand()%1;
-        if (f<p)
-        {
-            s[n][m]=-s[n][m];
-        }
         o=o+1;
-
     }
     ising.close ();
     return 0;
-
 }
 
 
